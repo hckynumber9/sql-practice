0d50dcc50f6ac1d3db63ed1281a6efabfed81817
Peter Schrader <peter.schrader2009@gmail.com>
2021-03-15 10:36:15 +0100

    more practice

--- misc/ca_practice.py
+++ misc/ca_practice.py
@@ -1,3 +1,5 @@
+# Practice SQL from Codecademy for review.
+
 CREATE TABLE friends (
   id INTEGER,
   name TEXT,
@@ -21,4 +23,59 @@ WHERE id=1;
 SELECT * FROM friends;
 
 -----------------------------------------
+
+SELECT imdb_rating AS 'IMDb'
+FROM movies;
+
+SELECT name,
+  CASE
+    WHEN genre='romance' THEN 'Chill'
+    WHEN genre='comedy' THEN 'Chill'
+    ELSE 'Intense'
+  END AS 'Mood'
+FROM movies;
+
+SELECT price,
+  ROUND(AVG(downloads)),
+  COUNT(*)
+FROM fake_apps
+GROUP BY price
+HAVING COUNT(*) > 10;
+
+SELECT CASE
+   WHEN url LIKE '%github.com%' THEN 'GitHub'
+   WHEN url LIKE '%medium.com%' THEN 'Medium'
+   WHEN url LIKE '%nytimes.com%' THEN 'New York Times'
+   ELSE 'Other'
+  END AS 'Source',
+  COUNT(*)
+FROM hacker_news
+GROUP BY 1;
+
+SELECT strftime('%H', timestamp) as 'Hour', 
+   ROUND(AVG(score), 1) as 'Average Score',
+   COUNT(*) as 'Stories'
+FROM hacker_news
+WHERE timestamp IS NOT NULL
+GROUP BY 1
+ORDER BY 1;
+
+-----------------------------------------
+
+SELECT *
+FROM table1
+UNION
+SELECT *
+FROM table2;
+
+WITH previous_query AS (
+  SELECT customer_id,
+    COUNT(subscription_id) AS 'subscriptions'
+  FROM orders
+  GROUP BY customer_id
+  )
+SELECT customers.customer_name, previous_query.subscriptions
+FROM previous_query
+JOIN customers
+ON previous_query.customer_id=customers.customer_id;
 
